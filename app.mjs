2206a5da79fda36f96615dbd6ceaf9bad4063523
Cory Althoff <cory@theselftaughtprogrammer.io>
2022-01-19 15:32:34 -0800

    add contacts and alert API

--- app.mjs
+++ app.mjs
@@ -1,16 +1,14 @@
 import express from 'express';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 import 'dotenv/config'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import '@vonage/server-sdk'
 
 
 let app = express();
-
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
@@ -20,6 +18,13 @@ const firebaseConfig = {
     appId:  process.env.APP_ID
 };
 
+const vonage = new Vonage({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET,
+    applicationId: process.env.APPLICATION_ID,
+    privateKey: process.env.PRIVATE_KEY
+})
+
 initializeApp(firebaseConfig);
 
 app.get('/', function(req, res){
@@ -27,16 +32,40 @@ app.get('/', function(req, res){
 })
 
 app.post('/create/contact', async function (req, res) {
-    const database = getDatabase();
-    set(ref(database, 'users/' + req.query.name), {
+    set(ref(getDatabase(), 'users/' + req.query.name), {
         number: req.query.number,
     });
     res.sendStatus(200)
 })
 
-app.get('/location', function(req, res){
+app.get('/get/contacts', function(req, res){
+    const db = getDatabase();
+    onValue(ref(db, 'users/'), (snapshot) => {
+        const data = snapshot.val();
+        res.json(data)
+    });
+})
+
+app.get('/send/alert', function(req, res){
     console.log(req.query.long)
     console.log(req.query.lat)
+    vonage.channel.send(
+        { "type": "sms", "number": TO_NUMBER },
+        { "type": "sms", "number": FROM_NUMBER },
+        {
+            "content": {
+                "type": "text",
+                "text": "This is an SMS text message sent using the Messages API"
+            }
+        },
+        (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(data.message_uuid);
+            }
+        }
+    );
 
 })
 
