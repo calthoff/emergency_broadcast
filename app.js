ebba9e11092feb0613f24bcf0b84c8f150a1c628
Cory Althoff <cory@theselftaughtprogrammer.io>
2022-02-07 10:52:46 -0800

    clean up code and add mongo env variable

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 let app = express()
-app.use( bodyParser.json() )
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(express.static('public'))
-
-mongoose.connect('mongodb+srv://coryalthoff:rKr3SG9d9hqLnLVB@cluster0.uostk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+// 'mongodb+srv://coryalthoff:rKr3SG9d9hqLnLVB@cluster0.uostk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority\''
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 const contactsSchema = new mongoose.Schema({
     name: String,
     number: Number
-});
+})
 const Contacts = mongoose.model('Contacts', contactsSchema)
 
 const vonage = new Vonage({
@@ -28,36 +33,40 @@ app.get('/', function(req, res){
     res.sendFile('index.html')
 })
 
-app.post('/contacts',  function(req, res) {
-    const contact = new Contacts({name: req.body.name})
+app.post('/contacts', function(req, res) {
+    const contact = new Contacts({
+        name: req.body.name
+    })
     contact.save()
     res.redirect('/')
 })
 
-app.get('/contacts', function(req, res){
-    Contacts.find({}, function(err, contacts){
-        if(err){
+app.get('/contacts', function(req, res) {
+    Contacts.find({}, function(err, contacts) {
+        if (err) {
             console.log(err)
-        }
-        else {
+        } else {
             res.json(contacts)
         }
     })
 })
 
-app.post('/alert', function(req, res){
+app.post('/alert', function(req, res) {
     let long = req.body['coordinates']['long']
     let lat = req.body['coordinates']['lat']
     let contacts = req.body['contacts']
     for (let i = 0; i <= contacts.length; i++) {
-        vonage.channel.send(
-            { 'type': 'sms', 'number': contacts[i].number},
-            { 'type': 'sms', 'number': process.env.FROM_NUMBER},
-            {
+        vonage.channel.send({
+                'type': 'sms',
+                "number": contacts[i].number
+            }, {
+                'type': 'sms',
+                "number": process.env.FROM_NUMBER
+            }, {
                 'content': {
                     'type': 'text',
                     'text': `SOS! Your friend is in an emergency! Their latitude is ${lat} and` +
-                        ` their longitude is ${long}!`
+                        `their longitude is ${long}!`
                 }
             },
             (err, data) => {
